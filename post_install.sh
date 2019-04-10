38ca9d6775c69dece15342d19a51d5de94ce4cad
Jed <jed@g5.lan>
2019-04-09 23:12:57 -0400

    .

--- post_install.sh
+++ post_install.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
+setenv ROOT /var/srv
+
+mkdir -p ${ROOT}
+
+cd ${ROOT}
+
 fetch https://codeload.github.com/jed-frey/open-zwave-control-panel/zip/master 
 unzip master
-cd open-zwave-control-panel-master/
+rm master
+
 fetch https://codeload.github.com/jed-frey/open-zwave/zip/master
 unzip master
-rm -rf open-zwave
-mv open-zwave-master/ open-zwave
-gmake -j4
-cd ..
-gmake -j4
+rm master
+
+#rm -rf open-zwave
+#mv open-zwave-master/ open-zwave
+#gmake -j4
+#cd ..
+#gmake -j4
+#cp -r open-zwave/config ./
