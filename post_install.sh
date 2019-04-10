2a0b3ddcfa54bdfdae8602836c10bcd8889a1ef8
Jed <jed@g5.lan>
2019-04-09 22:39:43 -0400

    .

--- post_install.sh
+++ post_install.sh
@@ -1,1 +1,9 @@
 #!/bin/sh
+
+fetch https://codeload.github.com/jed-frey/open-zwave-control-panel/zip/master 
+unzip master
+cd open-zwave-control-panel-master/
+fetch https://codeload.github.com/jed-frey/open-zwave/zip/master
+unzip master
+rm -rf open-zwave
+mv open-zwave-master/ open-zwave
